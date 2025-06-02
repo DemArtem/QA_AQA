9ca41caa6243786894b2a2ff06db859e79190975
DemArtem <demar.demidov@yandex.ru>
2025-06-02 21:52:03 +0300

    AQA

--- src/Task6/PhoneBook.java
+++ src/Task6/PhoneBook.java
@@ -10,6 +10,23 @@ import java.util.Map;
 тогда при запросе такой фамилии должны выводиться все телефоны.
  */
 class PhoneBook {
+    public static void main(String[] args) {
+        PhoneBook phoneBook = new PhoneBook();
+
+        phoneBook.add("Иванов", "+375(29)567-87-09");
+        phoneBook.add("Иванов", "+375(33)864-07-93");
+        phoneBook.add("Петров", "+375(25)573-90-76");
+        phoneBook.add("Сидоров", "+375(29)784-00-56");
+
+        List<String> ivanovPhones = phoneBook.get("Иванов");
+        System.out.println("Телефоны Иванова: " + ivanovPhones);
+
+        List<String> petrovPhones = phoneBook.get("Петров");
+        System.out.println("Телефоны Петрова: " + petrovPhones);
+
+        List<String> sidorovPhones = phoneBook.get("Сидоров");
+        System.out.println("Телефоны Сидорова: " + sidorovPhones);
+    }
     private Map<String, List<String>> phoneBook;
 
     public PhoneBook() {
--- src/Task6/StudentManager.java
+++ src/Task6/StudentManager.java
@@ -1,6 +1,9 @@
 package Task6;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
+import static Task6.StudentManager.*;
+import static Task6.StudentManager.printStudents;
 /*
 Создайте класс Student, содержащий следующие характеристики – имя, группа, курс, оценки по предметам.
 Создайте коллекцию, содержащую объекты класса Student. Следует написать метод, который удаляет студентов со средним баллом < 3,
@@ -13,6 +16,20 @@ class Student {
     private String group;
     private int course;
     private int[] grades;
+
+    public static void main(String[] args) {
+        Set<Student> students = new HashSet<>();
+
+        students.add(new Student("Иванов И.K.", "Группа 1", 1, new int[]{4, 5, 3}));
+        students.add(new Student("Петров O.П.", "Группа 2", 1, new int[]{2, 3, 4}));
+        students.add(new Student("Сидоров Д.С.", "Группа 1", 2, new int[]{2, 3, 2}));
+        students.add(new Student("Кузнецов К.З.", "Группа 2", 2, new int[]{5, 4, 3}));
+
+        removeLowPerformingStudents(students);
+        promoteStudents(students);
+        printStudents(students, 1);
+        printStudents(students, 2);
+    }
 
     public Student(String name, String group, int course, int[] grades) {
         this.name = name;
